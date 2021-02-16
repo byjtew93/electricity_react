d3a830ab7082ce3f6dd60739b011e56664bde081
byjtew93 <benjamin.lozes@protonmail.com>
2021-02-16 14:11:01 +0100

    Change scroll to scrollToTop()

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
-import {Link} from 'react-router-dom'
 import {Link as Scroller, animateScroll} from 'react-scroll'
 import './Navbar.css'
-import {Global} from "../Global";
 
 export function Navbar() {
 	return <nav>
@@ -16,7 +14,7 @@ export function Navbar() {
 			</Scroller>
 			{/*<div className="triangle_container"><div className="triangle-right"/></div>*/}
 			<div className="links_container">
-				<Scroller to="topbar" smooth={true} className="link">ACCUEIL</Scroller>
+				<Scroller to="#" smooth={true} className="link" onClick={animateScroll.scrollToTop}>ACCUEIL</Scroller>
 				<Scroller to="services" smooth={true} offset={-window.screen.height*.09} className="link">SERVICES</Scroller>
 				<Scroller to="contact" smooth={true} offset={-window.screen.height*.09} className="link">CONTACT</Scroller>
 			</div>
